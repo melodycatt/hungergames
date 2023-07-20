38310717ab831ccbd6535393747b57633047a77d
Your Name <edward.lenzner@gmail.com>
2023-07-21 08:33:58 +1000

    big poo

--- script.ts
+++ script.ts
@@ -1,5 +1,3 @@
-import { TemplateLiteral } from "typescript"
-
 interface PlayerStats {
     maxHealth: number
     health: any
@@ -30,6 +28,10 @@ class Player {
     }
 
     event() {
+
+    }
+
+    kill() {
 
     }
 
@@ -72,26 +74,53 @@ class Food extends Item {
 class Action {
     text : string;
     playersNeeded : number;
-    players : Array<Player>;
+    players : Array<Player> | undefined = undefined;
     lethal : boolean;
     itemNeeded : [Item, number];
     itemGained : [Item, number];
     itemLost : [Item, number];
-    constructor(text : string, playersNeeded : number, lethal : boolean, itemNeeded : [Item, number], itemGained : [Item, number], itemLost : [Item, number]) {
+    constructor(text : string, playersNeeded : number, lethal : boolean, itemNeeded : [Item, number] | string, itemGained : [Item, number] | string, itemLost : [Item, number] | string) {
         this.text = text;
         this.playersNeeded = playersNeeded;
         this.lethal = lethal;
-        this.itemNeeded = itemNeeded;
-        this.itemGained = itemGained;
-        this.itemLost = itemLost;
+        if (typeof itemNeeded == 'string') {
+            this.itemNeeded = eval(itemNeeded)
+        } else {
+            this.itemNeeded = itemNeeded;
+        }
+        if (typeof itemGained == 'string') {
+            this.itemGained = eval(itemGained)
+        } else {
+            this.itemGained = itemGained;
+        }
+        if (typeof itemLost == 'string') {
+            this.itemLost = eval(itemLost)
+        } else {
+            this.itemLost = itemLost;
+        }
+    }
+
+    run (players : Array<Player>) {
+        this.players = players;
+        if (this.lethal == true) {
+            players.slice(1).forEach((player, index) => {
+                player.kill();
+            });
+        }
+        if (this.itemGained[1] > 0) {
+            this.players[this.itemGained[1]].inv.push(this.itemGained[0]);
+        }
+        if (this.itemLost[1] > 0) {
+            this.players[this.itemLost[1]].inv.push(this.itemLost[0]);
+        }
     }
 }
 
 
 let Presets = {
     Actions: {
-        kills: "new Action(\"this.players[0] + 'kills' + this.players[1] + '.'\", 2, true, {Presets.Items.null}, {Presets.Items.null}, {Presets.Items.null})",
-        bread: "new Action(\"this.players[0] + 'gets a piece of bread.' + this.players[1]\", 2, true, {Presets.Items.null}, {Presets.Items.Food.Bread}, {Presets.Items.null})"
+        kills: "new Action(\"this.players[0] + 'kills' + this.players[1]\", 2, true, '[Presets.Items.null, 0]', '[Presets.Items.null, 0]', '[Presets.Items.null, 0]')",
+        bread: "new Action(\"this.players[0] + 'gets a piece of bread.'\", 2, true, '[Presets.Items.null, 0]', '[Presets.Items.Food.Bread, 0]', '[Presets.Items.null, 0]')"
     },
 
     Items: {
@@ -101,13 +130,23 @@ let Presets = {
         Food: {
             Bread: new Food('Bread', 1, 2)
         },
-        null: new Item('', 0, {})
+        null: new Item('', 0)
     }
 }
 
 function ranIntInterval(min : number, max : number) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
-console.
+console.log(Presets.Actions.kills);
+console.log(eval(Presets.Actions.bread));
 
-console.log(eval(Presets.Actions.kills))
+function init() {
+    let playersAlive : Player[] = [
+        new Player("Big Poo", 'unused'),
+        new Player("Izutsumi", 'unused'),
+        new Player("Hilber", 'unused'),
+        new Player("Liliaa", 'unused'),        
+    ];
+    let playersLeftInRound : Player[];
+
+}
--- testy.js
+++ testy.js
@@ -1,8 +1,2 @@
-let a = 1
-let b = 2
-let c = `"super crazy" + 
-a`
-console.log(eval(c))
-a = 2
-b = 1
-console.log(eval(c))
+a = [1,2,3,4,5]
+console.log(a.slice(1))
