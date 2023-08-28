9838bd22f959f556035938a7490d16fc3a31942c
Your Name <edward.lenzner@gmail.com>
2023-08-28 12:17:27 +1000

    im going to OIAUDGSJDKNGVU

--- scripts/js/utils.js
+++ scripts/js/utils.js
@@ -15,25 +15,25 @@ export function ranIntInterval(min, max) {
 //presets of items and events
 export let Presets = {
     Actions: {
-        spearStab: "new Action(\"this.players[0].name + ' stabs ' + this.players[1].name + ' with a Spear.'\", 2, '[Presets.Items.null, 0]', '[Presets.Items.null, 0]', '[Presets.Items.items.weapons.Spear, 1]')",
+        spearStab: "new Action(\"this.players[0].name + ' stabs ' + this.players[1].name + ' with a Spear.'\", 2, '[Presets.Items.null, 0]', '[Presets.Items.null, 0]', '[Presets.Items.items.Weapons.Spear, 1]')",
         treeClimb: "new Action(\"this.players[0].name + ' tries to climb a tree, but falls out of it and dies.'\", 2, '[Presets.Items.null, 0]', '[Presets.Items.null, 0]', '[Presets.Items.kill, 2]')",
-        bread: "new Action(\"this.players[0].name + ' gets a piece of bread.'\", 1, '[Presets.Items.null, 0]', '[Presets.Items.Food.Bread, 0]', '[Presets.Items.null, 0]')"
+        bread: "new Action(\"this.players[0].name + ' gets a piece of bread.'\", 1, '[Presets.Items.null, 0]', '[Presets.Items.items.Food.Bread, 0]', '[Presets.Items.null, 0]')"
     },
     Items: {
         Weapons: {
-            Spear: new items.weapon('Spear', 3, 45),
-            Dagger: new items.weapon('Dagger', 1, 20),
-            Sword: new items.weapon('Sword', 4, 55),
-            Bow: new items.weapon('Bow', 1, 35),
+            Spear: new items.Weapon('Spear', 3, 45),
+            Dagger: new items.Weapon('Dagger', 1, 20),
+            Sword: new items.Weapon('Sword', 4, 55),
+            Bow: new items.Weapon('Bow', 1, 35),
         },
         Food: {
-            Bread: new Food('Bread', 1, 2),
-            Apple: new Food('Apple', 1, 1),
-            Steak: new Food('Steak', 1, 3),
-            Berries: new Food('Berries', 1, 1),
-            Nuts: new Food('Nuts', 1, 1),
+            Bread: new items.Food('Bread', 1, 2),
+            Apple: new items.Food('Apple', 1, 1),
+            Steak: new items.Food('Steak', 1, 3),
+            Berries: new items.Food('Berries', 1, 1),
+            Nuts: new items.Food('Nuts', 1, 1),
         },
-        null: new Item('', 0),
-        kill: new items.weapon('killer', 10000, 10000),
+        null: new items.Item('', 0),
+        kill: new items.Weapon('killer', 10000, 10000),
     }
 };
