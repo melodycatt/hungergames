483d5420c17617df2db9c897e1487bdb18d8ac79
Your Name <edward.lenzner@gmail.com>
2023-08-28 12:22:40 +1000

    ok saying please isnt working

--- scripts/js/hungerGamesScript/main.js
+++ scripts/js/hungerGamesScript/main.js
@@ -3,6 +3,7 @@
 import { Player } from "./players.js"
 import { Presets } from "../utils.js"
 import * as items from "../items.js" 
+import { Action } from "../items.js"
 //this whole thing is a hunger games sim
 //if you wanna run this download deno its the equiv of node.js
 ;
@@ -29,65 +30,6 @@ class Food extends Item {
     }
 }
 //Action class declaration
-class Action {
-    //constructor stuff
-    action = document.createElement('div');
-    container = document.createElement('div');
-    constructor(text, playersNeeded, lethal, itemNeeded, itemGained, itemLost) {
-        //players involved
-        this.players = [];
-        this.text = text;
-        this.playersNeeded = playersNeeded;
-        this.lethal = lethal;
-        if (typeof itemNeeded == 'string') {
-            this.itemNeeded = eval(itemNeeded);
-        }
-        else {
-            this.itemNeeded = itemNeeded;
-        }
-        if (typeof itemGained == 'string') {
-            this.itemGained = eval(itemGained);
-        }
-        else {
-            this.itemGained = itemGained;
-        }
-        if (typeof itemLost == 'string') {
-            this.itemLost = eval(itemLost);
-        }
-        else {
-            this.itemLost = itemLost;
-        }
-    }
-    //does all the stuff the action does
-    run() {
-        //debug
-        this.generate();
-        //if lethal, get rid of all the players except the first one, might make it so which players die is specified later
-        if (this.lethal) {
-            //slice returns a copy of a section of an array. this returns everything but the first and kills each of the plauyers
-            this.players.slice(1).forEach((player, index) => {
-                player.kill();
-            });
-        }
-        //item stuff
-        if (this.itemGained[1] > 0) {
-            this.players[this.itemGained[1]].inv.push(this.itemGained[0]);
-        }
-        if (this.itemLost[1] > 0) {
-            this.players[this.itemLost[1]].inv.push(this.itemLost[0]);
-        }
-    }
-    generate() {
-        this.container.className = 'actionContainer'
-        section.appendChild(this.container)
-        for (let player of this.players) {
-            this.container.appendChild(player.img.cloneNode());
-        }
-        this.action.className = 'action';
-        this.action.innerHTML = eval(this.text);
-        this.container.appendChild(this.action);
-    }
-}
 //init players alive and stuff
 export let playersAlive = [
     new Player("Big Poo"),
