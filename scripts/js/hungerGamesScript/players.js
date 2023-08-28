30972ea1ebf1e5d17aa0573fa07f2baca3c11942
Your Name <edward.lenzner@gmail.com>
2023-08-28 12:54:13 +1000

    ijphkxzuu

--- scripts/js/hungerGamesScript/players.js
+++ scripts/js/hungerGamesScript/players.js
@@ -32,7 +32,7 @@ export class Player {
         //initialises a random event/action from preset ones ive made (go to the Action class definition for context ig??)
         let actionReal = eval(utils.randomProperty(utils.Presets.Actions));
         //makes sure that the amount of players needed for the chosen action is not greater than the amount of players left that are eligible to be part of an action
-        while (actionReal.playersNeeded > playersLeftInRound.length) {
+        while (actionReal.playersNeeded > playersAlive.length) {
             actionReal = eval(utils.randomProperty(utils.Presets.Actions));
         }
         //adds this to the action's list of players invoved
